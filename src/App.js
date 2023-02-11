fe7f34ea24cf67f8a28c849a03cde72b1f5a9544
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-02-11 16:30:55 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import axios from "axios";
+import "./styles/style.scss";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/about/About";
+import Footer from "./components/footer/Footer";
 
 function App() {
+  const [news, setNews] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://newsapi.org/v2/top-headlines?country=tr&category=${selectedCategory}&apiKey=31eeec8f8f4948178452985145b73ad3`
+      );
+      setProducts(response.data.articles.slice(0, 3));
+    };
+    fetchData();
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://newsapi.org/v2/top-headlines?country=tr&category=${selectedCategory}&apiKey=31eeec8f8f4948178452985145b73ad3`
+      );
+      setNews(response.data.articles.slice(3, 21));
+    };
+    fetchData();
+  }, [selectedCategory]);
+
+  const filteredNews = news.filter((newsItem) => {
+    return (
+      newsItem.description &&
+      newsItem.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="App__list">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home
+                  filteredNews={filteredNews}
+                  products={products}
+                  selectedCategory={selectedCategory}
+                  setSelectedCategory={setSelectedCategory}
+                />
+              }
+            />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </section>
+        <footer className="App__footer">
+          <Footer />
+        </footer>
+      </div>
+    </Router>
   );
 }
 
