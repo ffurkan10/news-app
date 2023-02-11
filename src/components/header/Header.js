9a9916020e9c9b7f7ab864890ceb795f573290c6
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-02-11 18:59:53 +0300

    update header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../input/Input";
 import { Link } from "react-router-dom";
 import * as AiIcons from "react-icons/ai";
